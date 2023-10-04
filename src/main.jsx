340d8c5a76a97d820801875dc22d8f8f7d86fe4c
Mulanga <sikhwethamulanga@gmail.com>
2023-10-04 20:02:47 +0200

    first

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Register from './Register.jsx'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Register/>
+   
+  </React.StrictMode>,
+)
